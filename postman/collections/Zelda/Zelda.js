23f1a92cdea5980b5315e46e07f9f11f67a87736
CarlosQA <carlostecnicowl@gmail.com>
2026-03-26 20:22:45 -0300

    Adiciona formulário de pesquisa e exibição de resultados para jogos da série Zelda

--- postman/collections/Zelda/Zelda.js
+++ postman/collections/Zelda/Zelda.js
@@ -1,12 +1,73 @@
 
-   
-class jogos {
-    
-    constructor(parameters) {
-    
+
+class Zelda {
+    constructor() {
+
     }
-    
-   static async getJogoByName(name) {
+    static async loadFormulario() {
+        const formulario = document.createElement('form');
+        formulario.setAttribute('id', 'zelda-form');
+        formulario.innerHTML = `
+            <label for="pesquisaTxt">Pesquise o Nome ou ID do Jogo:</label>
+            <input type="text" id="pesquisaTxt" name="PesquisaTXT" placeholder="Digite o nome ou ID do jogo">
+            <label><input type="radio" name="Pesquisa" value="id">Por ID</label>
+            <label><input type="radio" name="Pesquisa" value="name">Por Nome</label>
+            <button id="buscar" type="button" class="menu-toggle" onclick="Zelda.buscarJogo()">Buscar</button>
+            <button id="limpar" type="button" class="menu-toggle" onclick="Zelda.limpar()">Limpar</button>
+        `;
+        if (!document.getElementById('zelda-form')) {
+            mainContent.innerHTML = '<h2>Zelda</h2><p>Bem-vindo ao universo de Zelda!</p>';
+            mainContent.appendChild(formulario);
+        }
+        Jogos.tabelaJogoZelda();
+    }
+    static async buscarJogo() {
+        const nameInput = document.getElementById('pesquisaTxt');
+        const name = nameInput.value.trim();
+        const radio_buttons = document.getElementsByName('Pesquisa');
+        let searchType = null;
+        for (var i = 0; i < radio_buttons.length; i++) {
+            if (radio_buttons[i].checked) {
+                searchType = radio_buttons[i].value;
+                break;
+            }
+        }
+        if (!searchType && name.length === 0) {
+            alert('Por favor, \n- Preencha o campo de pesquisa  \n- Selecione o tipo de pesquisa (ID ou Nome).');
+            return;
+        } else if (name.length === 0) {
+            alert('Por favor, preencha o campo de pesquisa.');
+            return;
+        } else if (!searchType) {
+            alert('Por favor, selecione o tipo de pesquisa (ID ou Nome).');
+            return;
+        }
+        await Jogos.exibeMelhorPesquisa(searchType, name);
+    }
+    static limpar() {
+        const nameInput = document.getElementById('pesquisaTxt');
+        nameInput.value = '';
+        const radio_buttons = document.getElementsByName('Pesquisa');
+        for (var i = 0; i < radio_buttons.length; i++) {
+            if (radio_buttons[i].checked) {
+                radio_buttons[i].checked = false;
+            }
+        }
+        if (!document.getElementsByClassName('tabela')[0]) {
+            resultsContainer.innerHTML = '';
+            Jogos.tabelaJogoZelda();
+        }
+    }
+}
+
+
+class Jogos {
+
+    constructor(parameters) {
+
+    }
+
+    static async getJogoByName(name) {
         return fetch(`https://zelda.fanapis.com/api/games?name=${encodeURIComponent(name)}`)
             .then(response => {
                 if (!response.ok) {
@@ -24,7 +85,7 @@ class jogos {
                 return [];
             });
     }
-    
+
     static async getJogoByID(id) {
         const url = `https://zelda.fanapis.com/api/games/${encodeURIComponent(id)}`;
         console.log("URL:", url);
@@ -61,5 +122,50 @@ class jogos {
         }
     }
 
+    static async tabelaJogoZelda() {
+        const jogosList = await this.getJogos();
+        const table = document.createElement('table');
+        table.classList.add('tabela');
+        table.innerHTML = '<tr class="linha"><th class="coluna">ID</th><th class="coluna">Nome</th><th class="coluna">Descrição</th><th class="coluna">Desenvolvedora</th><th class="coluna">Editora</th><th class="coluna">Data de Lançamento</th></tr>';
+        jogosList.forEach((jogo) => {
+            table.innerHTML += `<tr class="linha">
+            <td class="coluna">${jogo.id}</td>
+            <td class="coluna">${jogo.name}</td>
+            <td class="coluna">${jogo.description && jogo.description.length > 100 ? jogo.description.substring(0, 100) + '...' : jogo.description || ''}</td>
+            <td class="coluna">${jogo.developer}</td>
+            <td class="coluna">${jogo.publisher || ''}</td>
+            <td class="coluna">${jogo.released_date}</td>
+            </tr>`;
+        });
+        resultsContainer.appendChild(table);
+    }
+
+    static async paragrafosJogoZelda(Jogo) {
+        const jogosList = await Jogo;
+        resultsContainer.innerHTML = '<h2>Zelda</h2><p>Bem-vindo ao universo de Zelda!</p>'
+        resultsContainer.innerHTML += `<p class="resposta"><strong>ID:</strong> ${jogosList.id}</p>`;
+        resultsContainer.innerHTML += `<p class="resposta"><strong>Nome:</strong> ${jogosList.name}</p>`;
+        if (jogosList.description !== "" && jogosList.description.length > 0) {
+            resultsContainer.innerHTML += `<p class="resposta"><strong>Descrição:</strong> ${jogosList.description}</p>`;
+        }
+        resultsContainer.innerHTML += `<p class="resposta"><strong>Desenvolvedora:</strong> ${jogosList.developer}</p>`;
+        if (jogosList.publisher !== "" && jogosList.publisher.length > 0) {
+            resultsContainer.innerHTML += `<p class="resposta"><strong>Editora:</strong> ${jogosList.publisher}</p>`;
+        }
+        resultsContainer.innerHTML += `<p class="resposta"><strong>Data de Lançamento:</strong> ${jogosList.released_date}</p>`;
+    }
+
+    static async exibeMelhorPesquisa(tipo, valor) {
+        switch (tipo) {
+            case 'id':
+                this.paragrafosJogoZelda(await this.getJogoByID(valor));
+                break;
+            case 'name':
+                this.paragrafosJogoZelda(await this.getJogoByName(valor));
+                break;
+            default:
+                alert('Tipo de pesquisa inválido. Por favor, selecione "ID" ou "Nome".');
+        }
+    }
 
 }
--- ter.js
+++ ter.js
@@ -3,13 +3,15 @@ const btn_zelda = document.getElementById('Zelda');
 const btn_clear = document.getElementById('clear');
 const title = document.querySelector('title');
 const mainContent = document.querySelector('main');
+const resultsContainer = document.getElementById('results-container');
+
 
 btn_starWars.addEventListener('click', (event) => {
     event.preventDefault();
     title.innerText = 'Star Wars';
     console.log('Star Wars');
     mainContent.innerHTML = '<h2>Star Wars</h2><p>Bem-vindo ao universo de Star Wars!</p>';
-    teste();
+    resultsContainer.innerHTML = '';
 });
 
 btn_zelda.addEventListener('click', async (event) => {
@@ -17,51 +19,24 @@ btn_zelda.addEventListener('click', async (event) => {
     title.innerText = 'Zelda';
     var JogoID = "5f6ce9d805615a85623ec2b9";
     const name = "The Legend of Zelda: Majora's Mask";
-    paragrafosJogoZelda(jogos.getJogoByName(name));
-    
+    // paragrafosJogoZelda(jogos.getJogoByName(name));
+    Zelda.loadFormulario();
+
 });
 btn_clear.addEventListener('click', (event) => {
     event.preventDefault();
     title.innerText = 'Treinamento em Teste de API';
     clearMainContent();
 });
-async function paragrafosJogoZelda(Jogo) {
-    const jogosList = await Jogo;
-    mainContent.innerHTML = '<h2>Zelda</h2><p>Bem-vindo ao universo de Zelda!</p>' 
-    mainContent.innerHTML += `<p class="resposta"><strong>ID:</strong> ${jogosList.id}</p>`;
-    mainContent.innerHTML += `<p class="resposta"><strong>Nome:</strong> ${jogosList.name}</p>`;
-    if (jogosList.description !== "" && jogosList.description.length > 0) {
-        mainContent.innerHTML += `<p class="resposta"><strong>Descrição:</strong> ${jogosList.description}</p>`;
-    }
-    mainContent.innerHTML += `<p class="resposta"><strong>Desenvolvedora:</strong> ${jogosList.developer}</p>`; 
-    if (jogosList.publisher !== "" && jogosList.publisher.length > 0) {
-        mainContent.innerHTML += `<p class="resposta"><strong>Editora:</strong> ${jogosList.publisher}</p>`;
-    }
-    mainContent.innerHTML += `<p class="resposta"><strong>Data de Lançamento:</strong> ${jogosList.released_date}</p>`;
-}
 
-async function tabelaJogoZelda() {
-    const jogosList = await jogos.getJogos();
-    mainContent.innerHTML = '<h2>Zelda</h2><p>Bem-vindo ao universo de Zelda!</p>';
-    const table = document.createElement('table');
-    table.classList.add('tabela');
-    table.innerHTML = '<tr class="linha"><th class="coluna">ID</th><th class="coluna">Nome</th><th class="coluna">Descrição</th><th class="coluna">Desenvolvedora</th><th class="coluna">Editora</th><th class="coluna">Data de Lançamento</th></tr>';
-    jogosList.forEach((jogo) => {
-        table.innerHTML += `<tr class="linha">
-            <td class="coluna">${jogo.id}</td>
-            <td class="coluna">${jogo.name}</td>
-            <td class="coluna">${jogo.description && jogo.description.length > 100 ? jogo.description.substring(0, 100) + '...' : jogo.description || ''}</td>
-            <td class="coluna">${jogo.developer}</td>
-            <td class="coluna">${jogo.publisher || ''}</td>
-            <td class="coluna">${jogo.released_date}</td>
-            </tr>`;
-    });
-    mainContent.appendChild(table);
 
-}
+
+
 function clearMainContent() {
     mainContent.innerHTML = '';
+    resultsContainer.innerHTML = '';
 }
+
 async function teste() {
     try {
         
